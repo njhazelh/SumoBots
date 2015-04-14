b7249222331ef7b9f92990ec75559d0a1fc1a2b8
carismoses <moses.c@husky.neu.edu>
2015-04-14 16:22:39 -0400

    Revert "ignore changes"
    This reverts commit 9b9773f6a6f4421a0d9e96bc06780b41482e2dda.

--- sumoBots2.py
+++ sumoBots2.py
@@ -57,11 +57,10 @@ def init(canvas):
     canvas.data["userBot"] = userBot
     canvas.data["sumoGrid"] = world.getSumoGrid()
 
-    # run value iteration
+    # run value iteration for computer bot
     gamma = .3
     eps = .1
     U = runValueIteration(world, compBot, userBot, gamma, eps)
-    canvas.data["U"] = U
     
     # redraw the canvas
     redraw(canvas)
@@ -186,10 +185,8 @@ def keyPressed(event):
         redraw(canvas)
 
         if (world.isGameOver() == False) and (compBot.isTurn()):
-            # dictionary where keys are (turn, compBot, userBot)
-            #U = canvas.data["U"]
-            #world.performBestAction(U)
-            #world.moveBot('East')
+            world.moveBot('East')
+  
             # toggle turn 
             compBot.toggleTurn()
             userBot.toggleTurn()
