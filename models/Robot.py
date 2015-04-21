47e7789ae8b46fcd0431dc2f3678554bc7caeb29
Nicholas Jones <njhazelh@gmail.com>
2015-04-21 00:45:06 -0400

    Added some comments.  More needed.

--- models/Robot.py
+++ models/Robot.py
@@ -35,9 +35,16 @@ class Robot:
         self.last_action = None
 
     def set_enemy(self, enemy):
+        """
+        Set the robot this robot is trying to push out of the ring.
+        :param enemy: The enemy robot.
+        """
         self.enemy = enemy
 
     def load_strategy(self):
+        """
+        Load the strategy that this robot is configured to use.
+        """
         self.strategy = STRATEGIES.enum_to_strategy(self, self.enemy, self.world, self.type)
 
     def act(self):
@@ -85,10 +92,12 @@ class Robot:
         if grid[x][y + 1] != -9:
             actions.append(ACTIONS.MOVE_SOUTH)
 
-        # I think the robot should be able to kill itself.  This will also
-        # show that we've taught the robot not to kill itself.
+        # I think the robot should be able to kill itself.  It will take
+        # longer to train, but it will show that we've taught
+        # the robot not to kill itself.
         # Adding this change would also mean that we have to add all states
         # to the world in world.init_grid.
+        #
         # if x + 1 < world.cols:
         # actions.append(ACTIONS.MOVE_EAST)
         # if x - 1 >= 0:
@@ -96,11 +105,20 @@ class Robot:
         # if y + 1 < world.rows:
         # actions.append(ACTIONS.MOVE_NORTH)
         # if y - 1 >= 0:
-        #     actions.append(ACTIONS.MOVE_SOUTH)
+        # actions.append(ACTIONS.MOVE_SOUTH)
 
         return actions
 
     def get_transition_model(self, world=None):
+        """
+        Get the transition model for this robot.
+        :param world: The world to use to find this transition model. It defaults
+            to self.world, but you can also pass in a world (as is done in
+            qLearning).
+        :return: The transition model of the robot consisting of a dictionary of
+            actions.  The value of each action is another dictionary containing the actions
+            that may result of trying to perform that action and their probabilities.
+        """
         trans_model = {}
         if world is None:
             world = self.world
@@ -121,7 +139,12 @@ class Robot:
         return trans_model
 
     def get_all_actions(self, world=None):
-
+        """
+        Get all the possible legal actions for every state in the world.
+        :param world: The world to search in. Defaults to self.world.
+        :return: A dictionary where the keys are states in world, and
+            the values are lists of the legal actions at those states.
+        """
         if world is None: world = self.world
 
         allActions = {}
