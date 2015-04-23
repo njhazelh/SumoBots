9b1d8df9899cee0cd50cbe6b8ad8773afe3a6905
Nicholas Jones <njhazelh@gmail.com>
2015-04-23 11:53:42 -0400

    deleted trash

--- trash/Sumo.py
+++
@@ -1,8 +0,0 @@
-from trash import SumoArena, World, Bot
-
-rows = cols = 30
-canvas = 1
-bot1 = Bot(cols / 2 - 5, rows / 2, 1, 1, "blue", canvas)
-bot2 = Bot(cols / 2 + 5, rows / 2, 1, 1, "green", canvas)
-world = World(rows, cols, bot1, bot2)
-arena = SumoArena(world)
